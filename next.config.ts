d53caf76039a2e3171c71c6a8bca3662e9f45e70
FADYxp <ffady354@gmail.com>
2025-09-27 04:12:00 +0300

    Fixx22

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -16,6 +15,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-
-module.exports = nextConfig;
 export default nextConfig;
